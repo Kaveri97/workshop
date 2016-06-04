d8b89b8541e7a8508742099d5007621ca5372a6d
kaveri97 <kaverimukundan2013@gmail.com>
2016-06-04 12:14:40 +0530

    App, working on sign out

--- workshop/urls.py
+++ workshop/urls.py
@@ -14,8 +14,12 @@ urlpatterns = [
         anonymous_required(auth_views.login),
         {'template_name': 'register/login.html'},
         name='login'),
+    url(r'^user/logout/$',
+        auth_views.logout,
+        {'template_name': 'register/logout.html'},
+        name='logout'),
     url(r'^$', Home.as_view(), name='home'),
     url(r'^register/', include('registration.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
 
 ]
