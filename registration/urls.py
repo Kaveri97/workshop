d5d1f30f90c27fb6bb43fb99b5637d28e6f23b8a
kaveri97 <kaverimukundan2013@gmail.com>
2016-06-07 12:57:26 +0530

    App, working on update profile and details view

--- registration/urls.py
+++ registration/urls.py
@@ -7,6 +7,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', UserRegistrationView.as_view(), name='register_user'),
- url(r'^chocolate/add', AddChocolateView.as_view(), name='add_chocolate')
+    url(r'^chocolate/add', AddChocolateView.as_view(), name='add_chocolate'),
+    url( r'^chocolate/info/(?P<choco_id>\d+)/$', ChocolateDetailsView.as_view(), name="chocolate_info"),
+    url(r'^user/profile/edit/$', UserProfileUpdateView.as_view(), name='edit'),
 
 ]
--- registration/views.py
+++ registration/views.py
@@ -5,6 +5,8 @@ from django.views.generic.edit import FormView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from registration.forms import *
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.http import Http404
 
 # Create your views here.
 class Home(ListView, ):
@@ -34,4 +36,32 @@ class AddChocolateView( FormView):
     def form_valid(self, form):
         form.save()
         return FormView.form_valid(self, form)
+
+class ChocolateDetailsView(DetailView):
+    template_name = "register/chocolate/chocolate_info.html"
+
+    def get_object( self, queryset = None):
+        choco_id = self.kwargs['choco_id']
+        obj = Chocolate.objects.get(chocolate_id=choco_id)
+        if obj:
+            return obj
+        else:
+            raise Http404("No details Found.")
+
+class CurrentUserMixin(object):
+    model = User
+
+    def get_object(self, *args, **kwargs):
+        try:
+            obj = super(CurrentUserMixin, self).get_object(*args, **kwargs)
+        except AttributeError:
+            obj = self.request.user
+
+        return obj
+
+class UserProfileUpdateView(LoginRequiredMixin, CurrentUserMixin, UpdateView):
+    model = User
+    fields = user_fields + user_extra_fields
+    template_name = 'edit.html'
+    success_url = '/register/user/profile/edit/'
 
